9478699cb027db4762671e86dea3ec4e7dd0eb5d
Subham Panda <im.sspanda10@gmail.com>
2020-05-14 12:22:20 +0530

    Prepared app for deployment

--- models/tourModel.js
+++ models/tourModel.js
@@ -174,11 +174,11 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
-tourSchema.post(/^find/, function (docs, next) {
-  console.log(`Query took ${Date.now() - this.start} milliseconds`);
-  //docs here is all the documents returned form the query
-  next();
-});
+// tourSchema.post(/^find/, function (docs, next) {
+//   console.log(`Query took ${Date.now() - this.start} milliseconds`);
+//   //docs here is all the documents returned form the query
+//   next();
+// });
 
 //AGGREGATION MIDDLEWARE
 // tourSchema.pre('aggregate', function (next) {
